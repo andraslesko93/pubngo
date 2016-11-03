b1baa3ba8844d6e632ab377590cf27c9b53cd910
andraslesko93 <andras.lesko.93@gmail.com>
2016-11-03 18:13:27 +0100

    Database tables implemented

--- pubs/models.py
+++ pubs/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+from django.db.models.fields.related import ManyToManyField
 
 class UserProfile(models.Model):
-   
     user = models.OneToOneField(User)
     local_picture = models.ImageField(upload_to='profile_images', blank=True, default='profile_images/default.png')
     picture_url=models.URLField(blank=True)
@@ -16,3 +16,24 @@ class UserProfile(models.Model):
     def __unicode__(self):
         return self.user.username
 
+class UserPreferences(models.Model):
+    #Ajanlorendszerhez, ez alapjan listaz
+    user = models.OneToOneField(User)
+    distance_importance = models.IntegerField() #mennyire fontos h kozel legyen
+    price_importance = models.IntegerField() #mennyire fontos, hogy olcso legyen
+    feeling = models.CharField(max_length = 10) #regexp arra, hogy kesdobalo//bulizos//beszelgetos ---> Akar Mihez van kedved ma este? 
+    #Feeling szuresi feltetel lesz, tobbi ertek alapja vmi pref matrixot szamol
+
+class Rating(models.Model):
+    user = models.ForeignKey(User) #letaroljuk, hogy ki adat hozza, vagy anonim?
+    price = models.IntegerField() #siman ft-ban egyenlore
+    feeling = models.CharField(max_length = 10) #regexp arra, hogy kesdobalo//bulizos//beszelgetos
+
+class Location(models.Model): #mehet akar majd ra google maps-es cucc is
+    x_chord = models.FloatField()
+    y_chord = models.FloatField()
+    
+class Pub(models.Model):
+    name = models.CharField(max_length = 150)
+    location = models.OneToOneField(Location)
+    ratings = ManyToManyField(Rating)

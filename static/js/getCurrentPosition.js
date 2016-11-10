9b754adf7b27d8baac3be34889f0830b907d7c9f
andraslesko93 <andras.lesko.93@gmail.com>
2016-11-11 00:33:03 +0100

    Async javascript call solved in postCurrentPosition

--- static/js/getCurrentPosition.js
+++ static/js/getCurrentPosition.js
@@ -1,25 +1,18 @@
-function getCurrentPosition()
-{
-	if (navigator.geolocation) 
-	{
-	    navigator.geolocation.getCurrentPosition(showPosition, function() {
-	      handleLocationError(true, infoWindow, map.getCenter());
-	    });
-	} 
-	else 
-	{
-	    // Browser doesn't support Geolocation
-	    handleLocationError(false, infoWindow, map.getCenter());
-	}
-	function showPosition(position) 
-	{
-		var currentPosition = {
-				lat : position.coords.latitude,
-				lng : position.coords.longitude
-		};
-		//console.log(currentPosition)
-		document.getElementById("newPubLat").value = position.coords.latitude;
-		document.getElementById("newPubLng").value = position.coords.longitude;
-	}
-	
+function getCurrentPosition() {
+    var deferred = $.Deferred();
+    // if geo location is supported
+    if(navigator.geolocation) {
+        // get current position and pass the results to getPostalCode or time out after 5 seconds if it fails
+        navigator.geolocation.getCurrentPosition(deferred.resolve, geoLocationError, {
+            timeout: 5000
+        });
+    } else {
+        //geo location isn't supported
+        console.log('Your browser does not support Geo Location.');
+    }
+    return deferred.promise();
+}
+		    
+function geoLocationError() {
+    console.log('Geo Location failed.');
 }
--- static/js/postCurrentPosition.js
+++ static/js/postCurrentPosition.js
@@ -1,30 +1,27 @@
 function postCurrentPosition()
 {
-	//console.log("preclicked");
-	getCurrentPosition();
-	//nem a legszebb megoldas, de le kellene valahogy futnia a getCurrentPosition()-ek
-	setTimeout(function(){
-		var button = $("#addANewPubButton");
-		console.log("clicked");
-		var formValues = {};
-		$.each($("form").serializeArray(), function (i, field) {
-		    formValues[field.name] = field.value;
-		});
-		console.log(formValues);
-	    $.ajax({
-	    	type: 'POST',
-	        url: "/post_current_position/",
-	        dataType: "json",
-	        data: {'csrfmiddlewaretoken': document.getElementsByName('csrfmiddlewaretoken')[0].value, 
-	        	'lat':formValues.lat,
-	        	'lng':formValues.lng,
-	        	'pubName': formValues.pubName
-	        	},
-	        success: function (result) {
-	          console.log("OK");
-	          }
-	    });
-	}, 2000);
-	
-	
+$.when(getCurrentPosition()).then(function(data, textStatus, jqXHR) {
+    console.log(data.coords.longitude, data.coords.latitude);
+    var button = $("#addANewPubButton");
+	console.log("clicked");
+	var formValues = {};
+	$.each($("form").serializeArray(), function (i, field) {
+	    formValues[field.name] = field.value;
+	});
+	console.log(formValues);
+	$.ajax({
+		type: 'POST',
+	    url: "/post_current_position/",
+	    dataType: "json",
+	    data: {'csrfmiddlewaretoken': document.getElementsByName('csrfmiddlewaretoken')[0].value, 
+	    	'lat':data.coords.latitude,
+	    	'lng':data.coords.longitude,
+	    	'pubName': formValues.pubName
+	    	},
+	    success: function (result) {
+	      console.log("OK");
+	      }
+	});	
+
+});
 }
